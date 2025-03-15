6436a0a3b551cb13a2d2d484a5e8aa1766f61f2e
adoxcol <onelittle1221@gmail.com>
2025-03-16 03:03:27 +0600

    created server.js with erorrs and created file1 html file

---
+++ server.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+const server = http.createServer((req, res) => {
+ 
+  const { url, method } = req;
+
+  
+  console.log(`Request received: ${method} ${url}`);
+
+
+  if (method === 'GET') {
+      let filePath;
+
+      
+      switch (url) {
+          case '/':
+              filePath = path.join(__dirname, 'public', 'file1.html');
+              break;
+          case '/route1':
+              filePath = path.join(__dirname, 'public', 'file2.html');
+              break;
+          case '/route2':
+              filePath = path.join(__dirname, 'public', 'file3.html');
+              break;
+          
+      }
+
+      
+      fs.readFile(filePath, (err, data) => {
+          if (err) {
+              res.writeHead(500, { 'Content-Type': 'text/plain' });
+              res.end('Internal Server Error');
+          } else {
+              res.writeHead(200, { 'Content-Type': 'text/html' });
+              res.end(data);
+          }
+      });
+  } else {
+     
+      res.writeHead(405, { 'Content-Type': 'text/plain' });
+      res.end('Method Not Allowed');
+  }
+});
+
+const port = 3000;
+server.listen(PORT,() => {
+    console.log(`Server running on port ${port}`);
+})
